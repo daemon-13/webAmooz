9c99fc95b2b24aa4cafaa277ec2901fedad8bc01
daemon__ <root13.exe@gmail.com>
2021-11-28 16:10:36 +0330

    episode-107-end

---
+++ modules/Cyaxaress/Course/Http/Controllers/SeasonController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Cyaxaress\Course\Http\Controllers;
+
+use Cyaxaress\Common\Responses\AjaxResponses;
+use Cyaxaress\Course\Http\Requests\SeasonRequest;
+use Cyaxaress\Course\Models\Season;
+use Cyaxaress\Course\Repositories\SeasonRepo;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class SeasonController extends Controller
+{
+    private $seasonRepo;
+    public function __construct(SeasonRepo $seasonRepo)
+    {
+        $this->seasonRepo = $seasonRepo;
+    }
+
+    public function store($course, SeasonRequest $request )
+    {
+        $this->seasonRepo->store($course, $request);
+        newFeedback();
+        return back();
+    }
+
+    public function edit($id)
+    {
+        $season = $this->seasonRepo->findByid($id);
+        return view('Courses::seasons.edit', compact('season'));
+    }
+
+    public function update($id, SeasonRequest $request)
+    {
+        $this->seasonRepo->update($id, $request);
+        newFeedback();
+        return back();
+    }
+
+    public function destroy($id)
+    {
+        $season =  $this->seasonRepo->findByid($id) ;
+        $season->delete();
+        return AjaxResponses::SuccessResponse();
+    }
+
+    public function accept($id)
+    {
+//        $this->authorize('change_confirmation_status', Course::class);
+        if ($this->seasonRepo->updateConfirmationStatus($id, Season::CONFIRMATION_STATUS_ACCEPTED)){
+            return AjaxResponses::SuccessResponse();
+        }
+
+        return AjaxResponses::FailedResponse();
+    }
+
+    public function reject($id)
+    {
+//        $this->authorize('change_confirmation_status', Season::class);
+        if ($this->seasonRepo->updateConfirmationStatus($id, Season::CONFIRMATION_STATUS_REJECTED)){
+            return AjaxResponses::SuccessResponse();
+        }
+
+        return AjaxResponses::FailedResponse();
+    }
+
+    public function lock($id)
+    {
+//        $this->authorize('change_confirmation_status', Season::class);
+        if ($this->seasonRepo->updateStatus($id, Season::STATUS_LOCKED)){
+            return AjaxResponses::SuccessResponse();
+        }
+
+        return AjaxResponses::FailedResponse();
+    }
+
+    public function unlock($id)
+    {
+//        $this->authorize('change_confirmation_status', Season::class);
+        if ($this->seasonRepo->updateStatus($id, Season::STATUS_OPENED)){
+            return AjaxResponses::SuccessResponse();
+        }
+
+        return AjaxResponses::FailedResponse();
+    }
+
+
+}
--- public/panel/js/js.js
+++ public/panel/js/js.js
@@ -216,7 +216,7 @@ function updateConfirmationStatus(event, route, message, status, field = 'confir
     if(confirm(message)){
         $.post(route, { _method: "PATCH", _token: $('meta[name="_token"]').attr('content') })
             .done(function (response) {
-                if (status = "تایید شده") {
+                if (status == "تایید شده") {
                     $(event.target).closest('tr').find('td.' + field).html("<span class='text-success'>" + status + "</span>");
                 }else{
                     $(event.target).closest('tr').find('td.' + field).html("<span class='text-error'>" + status + "</span>");
